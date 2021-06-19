3492c9d22725bcbb4a0ae35274d0557d8a4feedd
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-06-19 18:46:26 -0300

    implementando registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { UsuarioRegisterComponent } from './auth/register/usuario-register/usuario-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    UsuarioRegisterComponent
   ],
   imports: [
     BrowserModule,
